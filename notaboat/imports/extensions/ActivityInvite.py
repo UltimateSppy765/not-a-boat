6e4376653d8972d52dce8aa1125a36a3daf2326a
UltimateSppy765 <tapash2004@outlook.com>
2022-09-05 15:00:03 +0530

    Fix lint issues

--- notaboat/imports/extensions/ActivityInvite.py
+++ notaboat/imports/extensions/ActivityInvite.py
@@ -3,16 +3,27 @@ from discord import app_commands
 from discord.ext import commands
 from ..modules import itrchecks
 
+
 class ActivityInvite(commands.Cog):
     """Commands to create invites to activties in voice channels."""
+
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
 
     @app_commands.check(itrchecks.itr_owner_check)
     @app_commands.command(name="start-activity")
-    async def start_activity(self, interaction: discord.Interaction, channel: discord.VoiceChannel, activity: discord.Object) -> None:
-        await interaction.response.send_message(f"**Channel:** {channel.mention}\n**Activity ID:** {activity.id}", ephemeral=True)
+    async def start_activity(
+        self,
+        interaction: discord.Interaction,
+        channel: discord.VoiceChannel,
+        activity: discord.Object,
+    ) -> None:
+        await interaction.response.send_message(
+            f"**Channel:** {channel.mention}\n**Activity ID:** {activity.id}",
+            ephemeral=True,
+        )
         return
+
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(ActivityInvite(client))
